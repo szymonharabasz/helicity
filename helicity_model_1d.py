affe1dc122e5127879801a26ce91687f38aaf856
Szymon Harabasz <szymon.harabasz@gmail.com>
2023-12-23 13:28:44 +0100

    Implented simple fitting in a notebook

--- helicity_model_1d.py
+++ helicity_model_1d.py
@@ -59,7 +59,7 @@ def fit_simple(model, hist_data, hist_mc):
     train_y = hist_to_tensor(hist_data)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.05)
-    N_EPOCHS = 200
+    N_EPOCHS = 500
     for epoch in range(N_EPOCHS):
         optimizer.zero_grad()
 
@@ -69,26 +69,4 @@ def fit_simple(model, hist_data, hist_mc):
         loss.backward()
         optimizer.step()
 
-        print("Epoch: {}, loss: {}, param: {}".format(epoch, loss.item(), [param.item() for param in model.parameters()]))
-
-model = Helicity1d()
-params = model.parameters()
-for param in params:
-    print("param: ", param)
-
-ekin = 1580
-frame = Frame.HX
-bins = Bins.readFrom("ranges.yml")
-HIST_INDEX = 4
-
-filename_data_np = "063_088_ag158ag_3200A_accepted_np.dat"
-filename_MC_rho_heli0 = "mar19_diele_inmedium_heli0cm_ag1580ag_jver22_np_newCuts.dat"
-
-histMakerData_np = HistMaker1d(filename_data_np, "_data_np", bins, frame, ekin)
-histMakerMC_rho_heli0 = HistMaker1d(filename_MC_rho_heli0, "_MC_rho_heli0", bins, frame, ekin)
-
-histsData_np = histMakerData_np.make_hists  ()
-simple_model = Helicity1d()
-hist_data_simple = histsData_np[0][HIST_INDEX]
-hist_mc_simple = histMakerMC_rho_heli0.make_hists(0.0)[0][HIST_INDEX]
-fit_simple(simple_model, hist_data_simple, hist_mc_simple)
+       # print("Epoch: {}, loss: {}, param: {}".format(epoch, loss.item(), [param.item() for param in model.parameters()]))
