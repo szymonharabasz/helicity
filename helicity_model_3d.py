22b23e03201ee812507f38b1c759854234a04e5c
Szymon Harabasz <szymon.harabasz@gmail.com>
2023-12-28 16:44:36 +0100

    Refactored 1d model and added a module for 3d model

--- helicity_model_3d.py
+++ helicity_model_3d.py
@@ -4,7 +4,7 @@ from eventsreader import Frame
 from bins import Bins
 
 
-class Helicity1d(torch.nn.Module):
+class Helicity3d(torch.nn.Module):
     def __init__(self, learn_norm):
         """
         In the constructor we instantiate four parameters and assign them as
@@ -13,6 +13,8 @@ class Helicity1d(torch.nn.Module):
         super().__init__()
         self.learn_norm = learn_norm
         self.lambda_theta = torch.nn.Parameter(torch.ones(()))
+        self.lambda_phi = torch.nn.Parameter(torch.ones(()))
+        self.lambda_theta_phi = torch.nn.Parameter(torch.ones(()))
         if self.learn_norm:
             self.norm = torch.nn.Parameter(torch.ones(()))
 
@@ -22,65 +24,72 @@ class Helicity1d(torch.nn.Module):
         a Tensor of output data. We can use Modules defined in the constructor as
         well as arbitrary operators on Tensors.
         """
-        cosTheta = x[0]
-        weight = 1.0 + self.lambda_theta * cosTheta ** 2
+        theta = x[0]
+        phi = x[1]
+        weight = (1.0 + self.lambda_theta * torch.cos(theta) ** 2 +
+                  self.lambda_phi * torch.sin(theta) ** 2 * torch.cos(phi) +
+                  self.lambda_theta_phi * torch.sin(theta) * torch.cos(phi))
         if self.learn_norm:
             return torch.vstack([
-                cosTheta,
-                self.norm * weight * x[1],
-                self.norm * weight * x[2]])
+                theta,
+                phi,
+                self.norm * weight * x[2],
+                self.norm * weight * x[3]])
         else:
             return torch.vstack([
-                cosTheta, weight * x[1],
-                weight * x[2]])
+                theta,
+                phi,
+                weight * x[2],
+                weight * x[3]])
 
     def string(self):
         """
         Just like any class in Python, you can also define custom method on PyTorch modules
         """
-        return f'y = (1 + {self.lambda_theta.item()} x^2'
+        return (f'y = (1 + {self.lambda_theta.item()} cos(theta)^2 + {self.lambda_theta_phi.item()} sin(2 theta) cos(phi) '
+                f'+ {self.lambda_phi} sin(theta)^2 cos(2 phi)')
 
 
 def chi2_loss(pred_y, y):
-    mask1 = y[1] > 0
-    mask2 = pred_y[1] > 0
+    mask1 = y[2] > 0
+    mask2 = pred_y[2] > 0
     mask = torch.logical_and(mask1, mask2)
     indices = torch.nonzero(mask)
-    data = y[1][indices]
-    data_err = y[2][indices]
-    model = pred_y[1][indices]
+    data = y[2][indices]
+    data_err = y[3][indices]
+    model = pred_y[2][indices]
     data_integral = data.sum()
     model_integral = model.sum()
     data = data / data_integral
     data_err = data_err / data_integral
     model = model / model_integral
-    # print("y[1] ", y[1])
-    # print("y[1][indices] ", y[1][indices])
-    # return (((y[1][indices] - pred_y[1][indices])/y[2][indices]) ** 2).sum()
     return (((data - model) / data_err) ** 2).sum()
 
 
 def chi2_loss_learn_norm(pred_y, y):
-    mask1 = y[1] > 0
-    mask2 = pred_y[1] > 0
+    mask1 = y[2] > 0
+    mask2 = pred_y[2] > 0
     mask = torch.logical_and(mask1, mask2)
     indices = torch.nonzero(mask)
-    data = y[1][indices]
-    data_err = y[2][indices]
-    model = pred_y[1][indices]
+    data = y[2][indices]
+    data_err = y[3][indices]
+    model = pred_y[2][indices]
     return (((data - model) / data_err) ** 2).sum()
 
 
 def hist_to_tensor(hist):
-    n = hist.GetNbinsX()
+    nx = hist.GetNbinsX()
+    ny = hist.GetNbinsY()
     columns = []
-    for b in range(1, n + 1):
-        t = torch.tensor([
-            hist.GetBinCenter(b),
-            hist.GetBinContent(b),
-            hist.GetBinError(b)
-        ])
-        columns.append(t)
+    for bx in range(1, nx + 1):
+        for by in range(1, ny + 1):
+            t = torch.tensor([
+                hist.GetXaxis().GetBinCenter(bx),
+                hist.GetXaxis().GetBinCenter(by),
+                hist.GetBinContent(bx, by),
+                hist.GetBinError(bx, by)
+            ])
+            columns.append(t)
     result = torch.stack(columns).transpose(0, 1)
     return result
 
